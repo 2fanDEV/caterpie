62ccca67c1234baedc8b539baa11d8fe6f93055e
Tufan <tufan.oezen@protonmail.com>
2025-01-15 00:42:32 +0100

    add checking swapchain extensions for device creation

--- src/engine/configuration.rs
+++ src/engine/configuration.rs
@@ -1,13 +1,13 @@
-use std::ffi::{c_void, CString};
+use std::ffi::{c_void, CStr, CString};
 
 use ash::{
     vk::{
         ApplicationInfo, DebugUtilsMessageSeverityFlagsEXT, DebugUtilsMessageTypeFlagsEXT,
         DebugUtilsMessengerCallbackDataEXT, DebugUtilsMessengerCreateInfoEXT,
-        DebugUtilsMessengerEXT, DeviceCreateInfo, DeviceQueueCreateInfo, InstanceCreateFlags,
-        InstanceCreateInfo, PhysicalDevice, PhysicalDeviceFeatures, Queue, QueueFlags, SurfaceKHR,
-        EXT_DEBUG_UTILS_NAME, KHR_GET_PHYSICAL_DEVICE_PROPERTIES2_NAME,
-        KHR_PORTABILITY_ENUMERATION_NAME,
+        DebugUtilsMessengerEXT, DeviceCreateInfo, DeviceQueueCreateInfo, ExtensionProperties,
+        InstanceCreateFlags, InstanceCreateInfo, PhysicalDevice, PhysicalDeviceFeatures, Queue,
+        QueueFlags, SurfaceKHR, EXT_DEBUG_UTILS_NAME, KHR_GET_PHYSICAL_DEVICE_PROPERTIES2_NAME,
+        KHR_PORTABILITY_ENUMERATION_NAME, KHR_SWAPCHAIN_NAME,
     },
     Device, Entry, Instance,
 };
@@ -35,7 +35,7 @@ impl Configuration {
             .unwrap()
             .pick_physical_device()
             .unwrap()
-            .find_logical_device()
+            .create_logical_device()
             .unwrap()
             .build()
             .unwrap()
@@ -52,8 +52,10 @@ pub struct ConfigurationBuilder {
     logical_device: Option<Device>,
     graphics_queue: Option<Queue>,
     presentation_queue: Option<Queue>,
+    device_extensions: Vec<*const i8>,
     surface_instance: Option<ash::khr::surface::Instance>,
     surface: Option<SurfaceKHR>,
+    swapchain_support_details: Option<SwapchainSupportDetails>,
 
     debug_instance: Option<ash::ext::debug_utils::Instance>,
     debug_messenger: Option<DebugUtilsMessengerEXT>,
@@ -109,6 +111,39 @@ impl QueueFamilyIndices {
             }
 
             Some(queue_family_indices)
+        }
+    }
+}
+
+#[derive(Clone)]
+struct SwapchainSupportDetails { 
+    capabilities: ash::vk::SurfaceCapabilitiesKHR,
+    formats: Vec<ash::vk::SurfaceFormatKHR>,
+    present_modes: Vec<ash::vk::PresentModeKHR>,
+}
+
+impl SwapchainSupportDetails {
+    fn query_swapchain_support(
+        instance: Instance,
+        surface_instance: ash::khr::surface::Instance,
+        surface: SurfaceKHR,
+        physical_device: PhysicalDevice,
+    ) -> SwapchainSupportDetails {
+        unsafe {
+            let capabilities = surface_instance
+                .get_physical_device_surface_capabilities(physical_device, surface)
+                .unwrap();
+            let formats = surface_instance
+                .get_physical_device_surface_formats(physical_device, surface)
+                .unwrap();
+            let present_modes = surface_instance
+                .get_physical_device_surface_present_modes(physical_device, surface)
+                .unwrap();
+            SwapchainSupportDetails {
+                capabilities,
+                formats,
+                present_modes,
+            }
         }
     }
 }
@@ -175,6 +210,8 @@ impl ConfigurationBuilder {
                     .unwrap(),
             );
 
+            info!("Instance has been created!");
+
             self.debug_instance = Some(ash::ext::debug_utils::Instance::new(
                 self.vulkan_entry.as_ref().unwrap(),
                 self.instance.as_ref().unwrap(),
@@ -186,6 +223,7 @@ impl ConfigurationBuilder {
                     .create_debug_utils_messenger(&debug_messenger_create_info, None)
                     .unwrap(),
             );
+            info!("Debug messenger has been created!");
         }
         Ok(self)
     }
@@ -207,6 +245,7 @@ impl ConfigurationBuilder {
                 .unwrap(),
             );
         }
+        info!("Surface has been created");
         Ok(self)
     }
 
@@ -230,7 +269,7 @@ impl ConfigurationBuilder {
         }
     }
 
-    pub fn is_device_suitable(&self, physical_device: &PhysicalDevice) -> bool {
+    pub fn is_device_suitable(&mut self, physical_device: &PhysicalDevice) -> bool {
         let queue_family_indices = QueueFamilyIndices::find_queue_family_indices(
             self.instance.as_ref().unwrap().clone(),
             self.surface_instance.as_ref().unwrap().clone(),
@@ -238,8 +277,56 @@ impl ConfigurationBuilder {
             *physical_device,
         )
         .expect("Failed to gather queue family indices");
+        let mut adequate_swapchain = false; 
+        let extensions_enabled = self.check_device_extension_support(physical_device);
+        if extensions_enabled {
+            let swapchain_support_details = SwapchainSupportDetails::query_swapchain_support(
+                self.instance.as_ref().unwrap().clone(),
+                self.surface_instance.as_ref().unwrap().clone(),
+                self.surface.unwrap(),
+                *physical_device,
+            );
+            self.swapchain_support_details = Some(swapchain_support_details.clone());
+            adequate_swapchain = !swapchain_support_details.formats.is_empty() && !swapchain_support_details.present_modes.is_empty();
+        }
+
         println!("{:?}", queue_family_indices);
-        queue_family_indices.is_complete()
+        queue_family_indices.is_complete() &&  extensions_enabled && adequate_swapchain
+    }
+
+    fn check_device_extension_support(&mut self, physical_device: &PhysicalDevice) -> bool {
+        let device_extensions = vec![ash::khr::swapchain::NAME.to_str().unwrap()];
+        let mut flag = true;
+        unsafe {
+            let enumerate_device_extension_properties = self
+                .instance
+                .as_ref()
+                .unwrap()
+                .enumerate_device_extension_properties(*physical_device)
+                .unwrap();
+            let device_extension_properties: Vec<&str> = enumerate_device_extension_properties
+                .iter()
+                .map(|property| {
+                    property
+                        .extension_name_as_c_str()
+                        .unwrap()
+                        .to_str()
+                        .unwrap()
+                })
+                .collect::<Vec<&str>>();
+
+            for extension in device_extensions {
+                if !device_extension_properties.contains(&extension) {
+                    flag = false;
+                    println!("{:?}, {:?}", extension, device_extension_properties);
+                }
+            }
+        }
+
+        if flag { 
+            self.device_extensions.push(KHR_SWAPCHAIN_NAME.as_ptr());
+        }
+        flag
     }
 
     fn check_validation_layer_support(&self) -> Result<bool, &str> {
@@ -267,7 +354,7 @@ impl ConfigurationBuilder {
         Err("Validation Layers are not present on this machine")
     }
 
-    fn find_logical_device(&mut self) -> Result<&mut ConfigurationBuilder, &str> {
+    fn create_logical_device(&mut self) -> Result<&mut ConfigurationBuilder, &str> {
         let instance = self.instance.as_ref().unwrap();
         self.queue_family_indices = QueueFamilyIndices::find_queue_family_indices(
             instance.clone(),
@@ -278,7 +365,7 @@ impl ConfigurationBuilder {
         );
         unsafe {
             let queue_priorities = [1.0];
-            let queue_family_indices = self.queue_family_indices.as_ref().unwrap();
+            let queue_family_indices = self.queue_family_indices.unwrap();
             let queue_indices = [
                 queue_family_indices.graphics_queue.unwrap(),
                 queue_family_indices.presentation_queue.unwrap(),
@@ -298,25 +385,30 @@ impl ConfigurationBuilder {
 
             let device_create_info = DeviceCreateInfo::default()
                 .queue_create_infos(&device_queue_create_infos)
-                .enabled_features(self.physical_device_features.as_ref().unwrap());
+                .enabled_features(self.physical_device_features.as_ref().unwrap())
+                .enabled_extension_names(&self.device_extensions);
             self.logical_device = Some(
                 instance
                     .create_device(self.physical_device.unwrap(), &device_create_info, None)
                     .unwrap(),
             );
 
-            self.graphics_queue = self.find_device_queue(queue_family_indices.graphics_queue.unwrap());
-            self.presentation_queue = self.find_device_queue(queue_family_indices.presentation_queue.unwrap());
+            self.graphics_queue =
+                self.find_device_queue(queue_family_indices.graphics_queue.unwrap());
+            self.presentation_queue =
+                self.find_device_queue(queue_family_indices.presentation_queue.unwrap());
         }
         Ok(self)
     }
 
     fn find_device_queue(&mut self, queue_family_index: u32) -> Option<Queue> {
         unsafe {
-            Some(self.logical_device.as_ref().unwrap().get_device_queue(
-               queue_family_index, 
-                    0,
-            ))
+            Some(
+                self.logical_device
+                    .as_ref()
+                    .unwrap()
+                    .get_device_queue(queue_family_index, 0),
+            )
         }
     }
 
